76feb8da7d813652797b1895cb7039c1b78f7474
SAMoskalenko <sa.moskalenko@gmail.com>
2020-02-27 22:10:24 +0300

    add PostgreSQL

--- tradebot/settings/dev.py
+++ tradebot/settings/dev.py
@@ -1,5 +1,4 @@
 import environ
-from .base import *
 
 env = environ.Env()
 environ.Env.read_env('.env')
@@ -7,3 +6,14 @@ environ.Env.read_env('.env')
 DEBUG = True
 
 TELEGRAM_BOT_ID = env('TELEGRAM_BOT_ID')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('POSTGRES_NAME'),
+        'USER': env('POSTGRES_NAME'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': '5432',
+    }
+}
